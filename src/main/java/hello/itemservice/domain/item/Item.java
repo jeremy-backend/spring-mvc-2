7a1cd87abb943eef1bffc3a963b4b380a3d81fdb
TheMoon <woghd1008@gmail.com>
2026-09-10 17:43:52 +0900

    feat BeanValidation 한계 구현

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,9 +11,10 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
+    @NotNull //수정 요구사항 추가
     private Long id;
 
-    @NotBlank(message = "공백X")
+    @NotBlank
     private String itemName;
 
     @NotNull
@@ -21,7 +22,7 @@ public class Item {
     private Integer price;
 
     @NotNull
-    @Max(9999)
+//    @Max(9999) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
