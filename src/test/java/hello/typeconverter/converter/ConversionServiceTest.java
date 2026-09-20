918f1284a33b573a2cb7e412a7c6bebae3bbb568
TheMoon <woghd1008@gmail.com>
2026-09-20 19:56:46 +0900

    feat: 컨버전 서비스 - ConversionService 구현

---
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,34 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //사용
+        Integer result = conversionService.convert("10", Integer.class);
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+        conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(new IpPort("127.0.0.1", 8080));
+
+        String ipPort = conversionService.convert(new IpPort("127.0.0.1", 8080), String.class);
+        assertThat(ipPort).isEqualTo("127.0.0.1:8080");
+
+    }
+}
